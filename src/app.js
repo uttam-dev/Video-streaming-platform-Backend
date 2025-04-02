dfaec9634f2571500fbe766065196b412d021806
uttam prajapati <uttamp.dev@gmail.com>
2025-04-02 12:19:48 +0530

    add some middlewares in app and configure cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true,
+}));
+
+app.use(express.json({limit:"10kb"}))
+app.use(cookieParser())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("public"))
+
 
 export default app;
