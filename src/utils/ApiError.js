51b794163e6053ac795ed673ae0d06149f757244
uttam prajapati <uttamp.dev@gmail.com>
2025-04-05 11:31:21 +0530

    create class of api error

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,6 +1,18 @@
-class ApiError{
-    constructor()
-    {
-        
+class ApiError extends Error {
+    constructor(statusCode, messaage, errors = [], stack = "") {
+        super(messaage);
+        this.statusCode = statusCode;
+        this.message = messaage;
+        this.errors = errors;
+        this.success = false;
+        this.data = null;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this.this.constructor);
+        }
     }
 }
+
+export { ApiError };
