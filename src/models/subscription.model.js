05f32b473aade2523edb185c19fb9088b323b56f
uttam prajapati <uttamp.dev@gmail.com>
2025-07-14 19:02:34 +0530

    Rename subscriber field to subscribers in subscription model for clarity

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -2,12 +2,12 @@ import mongoose, { model, Schema } from "mongoose";
 
 const subscriptionSchema = new Schema(
     {
-        subscriber: {
-            type: Schema.Types.ObjectId,
+        subscribers: {
+            type: Schema.Types.ObjectId,//who subscribed user
             ref: "User",
         },
         channel: {
-            type: Schema.Types.ObjectId,
+            type: Schema.Types.ObjectId,//user subscribed others
             ref: "User",
         },
     },
