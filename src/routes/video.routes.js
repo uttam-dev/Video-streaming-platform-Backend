d89cef090d4f7620fb676c5959fb9eb443d9fee6
uttam prajapati <uttamp.dev@gmail.com>
2025-07-18 00:54:07 +0530

    Add video routes with JWT verification and CRUD operations

---
+++ src/routes/video.routes.js
@@ -0,0 +1,46 @@
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import {
+    getAllVideos,
+    publishAVideo,
+    getVideoById,
+    updateVideo,
+    updateThumbnail,
+    deleteVideo,
+    togglePublishStatus,
+} from "../controllers/video.controller.js";
+const router = Router();
+
+router.use(verifyJWT);
+
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1,
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1,
+            },
+        ]),
+        publishAVideo
+    );
+
+router
+    .route("/:videoId")
+    .get(getVideoById)
+    .delete(deleteVideo)
+    .patch(updateVideo);
+
+router
+    .route("/update/thumbnail/:videoId")
+    .patch(upload.single("thumbnail"), updateThumbnail);
+
+router.route("/toggle/publish/:videoId").patch(togglePublishStatus);
+
+export default router;
